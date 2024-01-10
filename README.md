626b204bff486250ae2b64a1575320aaae3510dc
Rutronik Elektronische Bauelemente GmbH <46448552+RutronikKaunas@users.noreply.github.com>
2024-01-10 13:55:30 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,72 @@
+# RDK2 RAB3 Radar Sensing
+
+This code example is a BGT60TR13C Radar *Presence Detection* and *Counting Persons* algorithms demonstration. The ModusToolbox project is based on the Infineon [xensiv-radar-sensing](https://github.com/Infineon/xensiv-radar-sensing) library and its documentation. 
+
+ <img src="images/rdk2_rab3.jpg" style="zoom:25%;" />
+
+## Requirements
+
+- [ModusToolbox® software](https://www.infineon.com/cms/en/design-support/tools/sdk/modustoolbox-software/) v3.1
+- RAB3-Radar Rev0 [prototype]
+
+## Using the code example
+
+Create the project and open it using one of the following:
+
+<details><summary><b>In Eclipse IDE for ModusToolbox&trade; software</b></summary>
+
+
+1. Click the **New Application** link in the **Quick Panel** (or, use **File** > **New** > **ModusToolbox&trade; Application**). This launches the [Project Creator](https://www.infineon.com/ModusToolboxProjectCreator) tool.
+
+2. Pick a kit supported by the code example from the list shown in the **Project Creator - Choose Board Support Package (BSP)** dialogue.
+
+   When you select a supported kit, the example is reconfigured automatically to work with the kit. To work with a different supported kit later, use the [Library Manager](https://www.infineon.com/ModusToolboxLibraryManager) to choose the BSP for the supported kit. You can use the Library Manager to select or update the BSP and firmware libraries used in this application. To access the Library Manager, click the link from the **Quick Panel**.
+
+   You can also just start the application creation process again and select a different kit.
+
+   If you want to use the application for a kit not listed here, you may need to update the source files. If the kit does not have the required resources, the application may not work.
+
+3. In the **Project Creator - Select Application** dialogue, choose the example by enabling the checkbox.
+
+4. (Optional) Change the suggested **New Application Name**.
+
+5. The **Application(s) Root Path** defaults to the Eclipse workspace which is usually the desired location for the application. If you want to store the application in a different location, you can change the *Application(s) Root Path* value. Applications that share libraries should be in the same root path.
+
+6. Click **Create** to complete the application creation process.
+
+For more details, see the [Eclipse IDE for ModusToolbox&trade; software user guide](https://www.infineon.com/MTBEclipseIDEUserGuide) (locally available at *{ModusToolbox&trade; software install directory}/docs_{version}/mt_ide_user_guide.pdf*).
+
+</details>
+
+### Operation
+
+The firmware example uses KitProg3 Debug UART for debug output. LED1 and LED2 are used to indicate the presence or absence of states in Presence Detection mode.
+
+<img src="images/data_output_pres.png" style="zoom:100%;" />
+
+The Counting Persons mode is enabled by uncommenting the definition as shown below
+
+```C
+/*******************************************************************************
+ * Macros
+ ********************************************************************************/
+//#define PRESENCE_DETECTION
+#define COUNTING_PERSONS
+```
+
+<img src="images/data_output_cnt.png" style="zoom:100%;" />
+
+### Debugging
+
+If you successfully have imported the example, the debug configurations are already prepared to use with the KitProg3 or MiniProg4. Open the ModusToolbox perspective and find the Quick Panel. Click on the desired debug launch configuration and wait for the programming to complete and the debugging process to start.
+
+<img src="images/debug.png" style="zoom:100%;" />
+
+## Legal Disclaimer
+
+The evaluation board including the software is for testing purposes only and, because it has limited functions and limited resilience, is not suitable for permanent use under real conditions. If the evaluation board is nevertheless used under real conditions, this is done at one’s responsibility; any liability of Rutronik is insofar excluded. 
+
+<img src="images/rutronik_origin_kaunas.png" style="zoom:50%;" />
+
+
+
---
+++ main.c
@@ -0,0 +1,438 @@
+/******************************************************************************
+* File Name:   main.c
+*
+* Description: This is the source code for the RDK2 RAB3 Radar Sensing
+*              Application for ModusToolbox.
+*
+* Related Document: See README.md
+*
+*
+*  Created on: 2024-01-10
+*  Company: Rutronik Elektronische Bauelemente GmbH
+*  Address: Jonavos g. 30, Kaunas 44262, Lithuania
+*  Author: GDR
+*
+* Rutronik Elektronische Bauelemente GmbH Disclaimer: The evaluation board
+* including the software is for testing purposes only and,
+* because it has limited functions and limited resilience, is not suitable
+* for permanent use under real conditions. If the evaluation board is
+* nevertheless used under real conditions, this is done at one’s responsibility;
+* any liability of Rutronik is insofar excluded
+*******************************************************************************/
+
+#include "cy_pdl.h"
+#include "cy_retarget_io.h"
+#include "cybsp.h"
+#include "cyhal.h"
+#include "mtb_radar_sensing.h"
+
+/*******************************************************************************
+ * Macros
+ ********************************************************************************/
+#define PRESENCE_DETECTION
+//#define COUNTING_PERSONS
+
+/* Timer resolution 1ms */
+#define TICKS_PER_SECOND 1000
+
+/* RADAR sensor SPI frequency */
+#define SPI_FREQUENCY (12500000UL)
+/** Pin for the shared RESET Signal */
+#define CYBSP_RESET (ARDU_IO4)
+/** Pin for the shared LDO EN Signal */
+#define CYBSP_LDO_EN (P2_6) //dummy pin for the stack, not used by the demo
+/** Pin for the shared IRQ Signal */
+#define CYBSP_IRQ (ARDU_IO6)
+
+/* LEDs */
+/* Pin number designated for LED RED */
+#define LED_RED (LED2)
+/* Pin number designated for LED GREEN */
+#define LED_GREEN (LED1)
+
+/* LED off */
+#define LED_STATE_OFF (1U)
+/* LED on */
+#define LED_STATE_ON (0U)
+
+#define UNUSED(x) (void)(x)
+
+/*******************************************************************************
+ * Function Prototypes
+ ********************************************************************************/
+void tick_timer_config();
+
+#if defined(COUNTING_PERSONS)
+    static void radar_counter_callback(mtb_radar_sensing_context_t *context,
+                                    mtb_radar_sensing_event_t event,
+                                    mtb_radar_sensing_event_info_t *event_info,
+                                    void *data);
+#else
+    static void radar_presence_sensing_callback(mtb_radar_sensing_context_t *context,
+                                                mtb_radar_sensing_event_t event,
+                                                mtb_radar_sensing_event_info_t *event_info,
+                                                void *data);
+#endif
+
+/*******************************************************************************
+ * Global Variables
+ ********************************************************************************/
+cyhal_timer_t tick_timer;
+static volatile uint32_t ticks = 0;
+
+static mtb_radar_sensing_mask_t application_type =
+#if defined(COUNTING_PERSONS)
+ MTB_RADAR_SENSING_MASK_COUNTER_EVENTS
+#else
+ MTB_RADAR_SENSING_MASK_PRESENCE_EVENTS
+#endif
+ ;
+
+static mtb_radar_sensing_callback_t callback_function =
+#if defined(COUNTING_PERSONS)
+ radar_counter_callback
+#else
+ radar_presence_sensing_callback
+#endif
+ ;
+
+static mtb_radar_sensing_context_t radar_context;
+
+/*Tick Timer Interrupt*/
+static void tick_isr(void *callback_arg, cyhal_timer_event_t event)
+{
+    (void) callback_arg;
+    (void) event;
+
+    ticks++;
+}
+
+/*******************************************************************************
+* Function Name: sys_now
+********************************************************************************
+* Summary:
+* Returns the current time in milliseconds.
+*
+* Parameters:
+*  none
+*
+* Return:
+*  the current time in milliseconds
+*
+*******************************************************************************/
+ uint64_t sys_now(void)
+{
+    return ticks;
+}
+
+/*******************************************************************************
+ * Function Name: main
+ ********************************************************************************
+ * Summary:
+ * This is the main function. It does:
+ *    1. Configure the board.
+ *    2. Initializing the radar.
+ *    3. Setting the callback function depending on the choosed application type: presence detection, counting persons.
+ *    4. Enabling the radar sensing.
+ *    5. Processing the radar events and printing the results on the console.
+ *
+ * Parameters:
+ *  void
+ *
+ * Return:
+ *  int
+ *
+ *******************************************************************************/
+int main(void)
+{
+    cy_rslt_t result;
+    cyhal_spi_t mSPI;
+
+    mtb_radar_sensing_hw_cfg_t hw_cfg = {.spi_cs = CYBSP_SPI_CS,
+                                         .reset = CYBSP_RESET,
+                                         .ldo_en = CYBSP_LDO_EN,
+                                         .irq = CYBSP_IRQ,
+                                         .spi = &mSPI};
+
+    /* Configure a 1ms Tick interrupt */
+    tick_timer_config();
+
+    result = cybsp_init();
+    CY_ASSERT(result == CY_RSLT_SUCCESS);
+
+    /* Enable global interrupts. */
+    __enable_irq();
+
+    /*Initialize Module RESET and keep it low*/
+    result = cyhal_gpio_init( ARDU_IO5, CYHAL_GPIO_DIR_OUTPUT, CYHAL_GPIO_DRIVE_STRONG, false);
+    if (result != CY_RSLT_SUCCESS)
+    {
+    	 CY_ASSERT(0);
+    }
+    /*Initialize BGT60TR13C Power Control pin*/
+    result = cyhal_gpio_init(ARDU_IO3, CYHAL_GPIO_DIR_OUTPUT, CYHAL_GPIO_DRIVE_STRONG, false); /*Keep it OFF*/
+    if (result != CY_RSLT_SUCCESS)
+    {
+    	CY_ASSERT(0);
+    }
+
+    /* Initialize two LED ports and set LEDs' initial state to off */
+    result = cyhal_gpio_init(LED_RED, CYHAL_GPIO_DIR_OUTPUT, CYHAL_GPIO_DRIVE_STRONG, LED_STATE_OFF);
+    if (result != CY_RSLT_SUCCESS)
+    {
+        CY_ASSERT(0);
+    }
+
+    result = cyhal_gpio_init(LED_GREEN, CYHAL_GPIO_DIR_OUTPUT, CYHAL_GPIO_DRIVE_STRONG, LED_STATE_OFF);
+    if (result != CY_RSLT_SUCCESS)
+    {
+        CY_ASSERT(0);
+    }
+
+    /* Initialize retarget-io to use the debug UART port */
+    result = cy_retarget_io_init(CYBSP_DEBUG_UART_TX, CYBSP_DEBUG_UART_RX, CY_RETARGET_IO_BAUDRATE);
+    CY_ASSERT(result == CY_RSLT_SUCCESS);
+
+    /* Activate radar reset pin */
+    result = cyhal_gpio_init(hw_cfg.reset, CYHAL_GPIO_DIR_OUTPUT, CYHAL_GPIO_DRIVE_STRONG, true);
+    CY_ASSERT(result == CY_RSLT_SUCCESS);
+
+    /* Enable LDO */
+    result = cyhal_gpio_init(hw_cfg.ldo_en, CYHAL_GPIO_DIR_OUTPUT, CYHAL_GPIO_DRIVE_STRONG, true);
+    CY_ASSERT(result == CY_RSLT_SUCCESS);
+
+    /* Enable IRQ pin */
+    result = cyhal_gpio_init(hw_cfg.irq, CYHAL_GPIO_DIR_INPUT, CYHAL_GPIO_DRIVE_PULLDOWN, false);
+    CY_ASSERT(result == CY_RSLT_SUCCESS);
+
+    /* CS handled manually */
+    result = cyhal_gpio_init(hw_cfg.spi_cs, CYHAL_GPIO_DIR_OUTPUT, CYHAL_GPIO_DRIVE_STRONG, true);
+    CY_ASSERT(result == CY_RSLT_SUCCESS);
+
+    /* Configure SPI interface */
+    if (cyhal_spi_init(hw_cfg.spi,
+                       CYBSP_SPI_MOSI,
+                       CYBSP_SPI_MISO,
+                       CYBSP_SPI_CLK,
+                       NC,
+                       NULL,
+                       8,
+                       CYHAL_SPI_MODE_00_MSB,
+                       false) != CY_RSLT_SUCCESS)
+        CY_ASSERT(result == CY_RSLT_SUCCESS);
+
+    /* Reduce drive strength to improve EMI */
+    Cy_GPIO_SetSlewRate(CYHAL_GET_PORTADDR(CYBSP_SPI_MOSI), CYHAL_GET_PIN(CYBSP_SPI_MOSI), CY_GPIO_SLEW_FAST);
+    Cy_GPIO_SetDriveSel(CYHAL_GET_PORTADDR(CYBSP_SPI_MOSI), CYHAL_GET_PIN(CYBSP_SPI_MOSI), CY_GPIO_DRIVE_1_8);
+    Cy_GPIO_SetSlewRate(CYHAL_GET_PORTADDR(CYBSP_SPI_CLK), CYHAL_GET_PIN(CYBSP_SPI_CLK), CY_GPIO_SLEW_FAST);
+    Cy_GPIO_SetDriveSel(CYHAL_GET_PORTADDR(CYBSP_SPI_CLK), CYHAL_GET_PIN(CYBSP_SPI_CLK), CY_GPIO_DRIVE_1_8);
+
+    /* Set the data rate to 12.5 Mbps */
+    if (cyhal_spi_set_frequency(hw_cfg.spi, SPI_FREQUENCY) != CY_RSLT_SUCCESS)
+        CY_ASSERT(result == CY_RSLT_SUCCESS);
+
+    /* Radar initialization */
+    result = mtb_radar_sensing_init(&radar_context, &hw_cfg, application_type);
+    if (CY_RSLT_SUCCESS != result)
+    {
+        printf("ERROR Radar Sensing init.\n\r");
+        CY_ASSERT(0);
+    }
+
+    /* Register radar event callback */
+    result = mtb_radar_sensing_register_callback(&radar_context, callback_function, NULL);
+    if (CY_RSLT_SUCCESS != result)
+    {
+        printf("ERROR Radar Registering Callback.\n\r");
+        CY_ASSERT(0);
+    }
+
+    /* Enable radar sensing */
+    result = mtb_radar_sensing_enable(&radar_context);
+    if (CY_RSLT_SUCCESS != result)
+    {
+        printf("ERROR Radar Enabling.\n\r");
+        CY_ASSERT(0);
+    }
+
+    /* \x1b[2J\x1b[;H - ANSI ESC sequence for clear screen */
+    printf("\x1b[2J\x1b[;H");
+
+#if defined(COUNTING_PERSONS)
+    printf("*************************** "
+           "Radar Counting Persons Example "
+           "*************************** \r\n\n");
+#else
+    printf("*************************** "
+           "Radar Presence Example "
+           "*************************** \r\n\n");
+    /*Initial indication state is ABSENCE*/
+    cyhal_gpio_write(LED_RED, LED_STATE_OFF);
+    cyhal_gpio_write(LED_GREEN, LED_STATE_ON);
+#endif
+
+    for (;;)
+    {
+     /* Radar data processing */
+        if (mtb_radar_sensing_process(&radar_context, sys_now()) != CY_RSLT_SUCCESS)
+        {
+            printf("cskit_radar_sensing_process error\n\r");
+            CY_ASSERT(0);
+        }
+
+        cyhal_system_delay_ms(MTB_RADAR_SENSING_PROCESS_DELAY);
+    }
+}
+
+/*******************************************************************************
+* Function Name: radar_counter_callback
+********************************************************************************
+* Summary:
+* Printing events for counter event and controlling the leds state.
+*
+* Parameters:
+*  context: Pointer to context of radar
+*   event: Event type
+*   event_info: Pointer to structure holding the informations about the event
+*   data: Pointer to data
+*
+* Return:
+*  none
+*
+*******************************************************************************/
+#if defined(COUNTING_PERSONS)
+static void radar_counter_callback(mtb_radar_sensing_context_t *context,
+                                   mtb_radar_sensing_event_t event,
+                                   mtb_radar_sensing_event_info_t *event_info,
+                                   void *data)
+{
+    UNUSED(context);
+    UNUSED(data);
+
+    switch (event)
+    {
+        case MTB_RADAR_SENSING_EVENT_COUNTER_IN:
+            cyhal_gpio_write(LED_RED, LED_STATE_ON);
+            cyhal_gpio_write(LED_GREEN, LED_STATE_OFF);
+            printf("%.2f: Counter IN detected, IN: %d, OUT: %d\n\r",
+                   (float)event_info->timestamp / 1000,
+                   ((mtb_radar_sensing_counter_event_info_t *)event_info)->in_count,
+                   ((mtb_radar_sensing_counter_event_info_t *)event_info)->out_count);
+            break;
+        case MTB_RADAR_SENSING_EVENT_COUNTER_OUT:
+            cyhal_gpio_write(LED_RED, LED_STATE_OFF);
+            cyhal_gpio_write(LED_GREEN, LED_STATE_ON);
+            printf("%.2f: Counter OUT detected, IN: %d, OUT: %d\n\r",
+                   (float)event_info->timestamp / 1000,
+                   ((mtb_radar_sensing_counter_event_info_t *)event_info)->in_count,
+                   ((mtb_radar_sensing_counter_event_info_t *)event_info)->out_count);
+            break;
+        case MTB_RADAR_SENSING_EVENT_COUNTER_OCCUPIED:
+            cyhal_gpio_write(LED_RED, LED_STATE_ON);
+            cyhal_gpio_write(LED_GREEN, LED_STATE_OFF);
+            printf("%.2f: Counter occupied detected, IN: %d, OUT: %d\n\r",
+                   (float)event_info->timestamp / 1000,
+                   ((mtb_radar_sensing_counter_event_info_t *)event_info)->in_count,
+                   ((mtb_radar_sensing_counter_event_info_t *)event_info)->out_count);
+            break;
+        case MTB_RADAR_SENSING_EVENT_COUNTER_FREE:
+            cyhal_gpio_write(LED_RED, LED_STATE_OFF);
+            cyhal_gpio_write(LED_GREEN, LED_STATE_ON);
+            printf("%.2f: Counter free detected, IN: %d, OUT: %d\n\r",
+                   (float)event_info->timestamp / 1000,
+                   ((mtb_radar_sensing_counter_event_info_t *)event_info)->in_count,
+                   ((mtb_radar_sensing_counter_event_info_t *)event_info)->out_count);
+            break;
+        default:
+        {
+            printf("ERROR - Unknown event.\n\r");
+        }
+        break;
+    }
+}
+#else
+
+/*******************************************************************************
+* Function Name: radar_presence_sensing_callback
+********************************************************************************
+* Summary:
+* Printing events for presence event and controlling the leds state.
+*
+* Parameters:
+*  context: Pointer to context of radar
+*   event: Event type
+*   event_info: Pointer to structure holding the informations about the event
+*   data: Pointer to data
+*
+* Return:
+*  none
+*
+*******************************************************************************/
+static void radar_presence_sensing_callback(mtb_radar_sensing_context_t *context,
+                                            mtb_radar_sensing_event_t event,
+                                            mtb_radar_sensing_event_info_t *event_info,
+                                            void *data)
+{
+    UNUSED(context);
+    UNUSED(data);
+
+    switch (event)
+    {
+        case MTB_RADAR_SENSING_EVENT_PRESENCE_IN:
+        {
+            cyhal_gpio_write(LED_RED, LED_STATE_ON);
+            cyhal_gpio_write(LED_GREEN, LED_STATE_OFF);
+            printf("%.3f: Presence IN %.2f-%.2f\n\r",
+                   (float)event_info->timestamp / 1000,
+                   (float)(((mtb_radar_sensing_presence_event_info_t *)event_info)->distance -
+                           ((mtb_radar_sensing_presence_event_info_t *)event_info)->accuracy),
+                   (float)(((mtb_radar_sensing_presence_event_info_t *)event_info)->distance +
+                           ((mtb_radar_sensing_presence_event_info_t *)event_info)->accuracy));
+        }
+        break;
+        case MTB_RADAR_SENSING_EVENT_PRESENCE_OUT:
+        {
+            cyhal_gpio_write(LED_RED, LED_STATE_OFF);
+            cyhal_gpio_write(LED_GREEN, LED_STATE_ON);
+            printf("%.3f: Presence OUT\n\r", (float)event_info->timestamp / 1000);
+        }
+        break;
+        default:
+        {
+            printf("ERROR - Unknown event.\n\r");
+        }
+        break;
+    }
+}
+#endif
+
+/*Initialize timer for 1 ms Interrupts*/
+void tick_timer_config()
+{
+    cy_rslt_t rslt;
+    const cyhal_timer_cfg_t timer_cfg =
+    {
+        .compare_value = 0,                 /* Timer compare value, not used */
+        .period = 9999,                      /* Defines the timer period */
+        .direction = CYHAL_TIMER_DIR_UP,    /* Timer counts up */
+        .is_compare = false,                /* Don't use compare mode */
+        .is_continuous = true,              /* Run the timer indefinitely */
+        .value = 0                          /* Initial value of counter */
+    };
+    /* Initialize the timer object. Does not use pin output ('pin' is NC) and
+     * does not use a pre-configured clock source ('clk' is NULL). */
+    rslt = cyhal_timer_init(&tick_timer, NC, NULL);
+    CY_ASSERT(CY_RSLT_SUCCESS == rslt);
+    /* Apply timer configuration such as period, count direction, run mode, etc. */
+    rslt = cyhal_timer_configure(&tick_timer, &timer_cfg);
+    /* Set the frequency of timer to 10000 Hz */
+    rslt = cyhal_timer_set_frequency(&tick_timer, 1000000);
+    /* Assign the ISR to execute on timer interrupt */
+    cyhal_timer_register_callback(&tick_timer, tick_isr, NULL);
+    /* Set the event on which timer interrupt occurs and enable it */
+    cyhal_timer_enable_event(&tick_timer, CYHAL_TIMER_IRQ_TERMINAL_COUNT, 3, true);
+    /* Start the timer with the configured settings */
+    rslt = cyhal_timer_start(&tick_timer);
+}
+
+/* [] END OF FILE */
